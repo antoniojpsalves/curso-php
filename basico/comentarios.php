30434066be2d7b88571d0ef023266b09cc78ee12
Antonio <antonio.jpsalves@gmail.com>
2021-08-10 22:44:33 -0300

    Commit inicial do curso de php

---
+++ basico/comentarios.php
@@ -0,0 +1,22 @@
+<div class="titulo">Comentários em PHP</div>
+
+
+<?php
+    echo "Estou no PHP";
+    //echo "Estou no PHP";
+    #echo "Estou no PHP";
+    #echo //"Estou no PHP";
+    //echo #"Estou no PHP";
+?>
+
+<p>Depois do primeiro bloco!</p>
+
+<?php
+    /*
+        primeira linha do comentário
+        segunda linha do comentário
+        terceira linha do comentário
+    */
+?>
+
+<p>Depois do segundo bloco!</p>
---
+++ basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Olá PHP</div>
+
+
+<h2>Exemplo A:</h2>
+<?php 
+    echo 'Hello World';
+?>
+
+<h2>Exemplo B:</h2>
+<?= "Prática em linguagem PHP" ?>
+
+<?php
+//phpinfo();
