fc32d36c5fe749282c4444e527b0271c605ac327
Antonio <antonio.jpsalves@gmail.com>
2021-08-10 23:14:31 -0300

    Representação dos valores inteiros no php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá em PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
@@ -29,9 +29,9 @@
                     </ul>
                 </div>
                 <div class="modulo vermelho">
-                    <h3>Módulo 02</h3>
+                    <h3>Tipos</h3>
                     <ul>
-                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a></li>
                         <li><a href="#">Exercício B</a></li>
                         <li><a href="#">Exercício C</a></li>
                     </ul>
---
+++ tipos/int.php
@@ -0,0 +1,16 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+    echo 11; // valor literal
+    echo '<br>';
+
+    var_dump(11); //função que apresenta informações de variáveis.
+    echo '<br>';
+
+    echo PHP_INT_MAX, '<br>'; //Informação sobre o valor inteiro máximo de acordo com o sistema 64bits*.
+    echo PHP_INT_MIN, '<br>'; //Metade negativa. Incluíndo o ZERO*.
+
+    echo 017, '<br>'; //Representação de um número em base OCTAL*.
+    echo 0b11, '<br>'; //Representação de um número na base BINÁRIA*.
+    echo 0x117acf0, '<br>'; //Representação de um número na base HEXADECIMAL*.
+     
